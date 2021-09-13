896af5eb42b95f3fe6e6c6f58572d0ca1a09306c
Erick Vicentin <erickvicentin.globant@latam.com>
2021-09-13 19:03:22 -0300

    Added exercises until 1.36

--- tp3-scripts-grupo5.sql
+++ tp3-scripts-grupo5.sql
@@ -117,17 +117,96 @@ GROUP BY t1.date_rental;
 #1.25)
 CREATE VIEW vw_promedio_dias AS 
 (SELECT film_id AS Pelicula, AVG(TIMESTAMPDIFF(DAY, r.rental_date, r.return_date)) AS Promedios
- FROM (rental AS r JOIN inventory AS i USING(inventory_id)) JOIN film AS f USING(film_id)
- WHERE r.return_date IS NOT NULL
- GROUP BY film_id);
+FROM (rental AS r JOIN inventory AS i USING(inventory_id)) JOIN film AS f USING(film_id)
+WHERE r.return_date IS NOT NULL
+GROUP BY film_id);
  
- SELECT * FROM vw_promedio_dias;
+SELECT * FROM vw_promedio_dias;
  
- #1.26)
- CREATE VIEW vw_empleados_pagos AS
- (SELECT staff_id AS Empleado, COUNT(payment_id) Cantidad_Pagos
-  FROM staff AS s JOIN payment AS p USING(staff_id)
-  GROUP BY staff_id 
-  HAVING COUNT(*) > 10);
+#1.26)
+CREATE VIEW vw_empleados_pagos AS
+(SELECT staff_id AS Empleado, COUNT(payment_id) Cantidad_Pagos
+FROM staff AS s JOIN payment AS p USING(staff_id)
+GROUP BY staff_id 
+HAVING COUNT(*) > 10);
   
-  SELECT * FROM vw_empleados_pagos;
+SELECT * FROM vw_empleados_pagos;
+  
+#1.27)
+SELECT c.first_name AS Nombre, c.last_name AS Apellido, a.address AS Domicilio, a.phone AS Telefono
+FROM address AS a JOIN customer AS c USING(address_id)
+WHERE a.address REGEXP '[0-9] A'
+LIMIT 10;
+  
+SELECT c.first_name AS Nombre, c.last_name AS Apellido, a.address AS Domicilio, a.phone AS Telefono
+FROM address AS a JOIN customer AS c USING(address_id)
+WHERE a.address LIKE 'A%' AND a.phone = ''
+LIMIT 10;
+  
+#1.28)
+SELECT r.rental_date, c.first_name, c.last_name, f.title, s.first_name, s.last_name
+FROM rental AS r, film AS f, inventory AS i, customer AS c, staff AS s
+WHERE r.inventory_id = i.inventory_id AND
+	  r.customer_id = c.customer_id AND
+	  r.staff_id = s.staff_id AND
+	  i.film_id = f.film_id
+LIMIT 20;
+
+#1.29)
+SELECT r.rental_id AS RentalID, datediff(return_date, rental_date) AS DiasPrestado, p.amount AS monto
+FROM rental AS r, payment AS p
+WHERE r.rental_id = p.rental_id
+LIMIT 10;
+
+#1.30)
+SELECT c.customer_id AS Cliente, c.first_name AS Nombre, c.last_name AS Apellido, r1.rental_id AlquierNro
+FROM ((SELECT r.customer_id, SUM(p.amount)
+	   FROM rental r JOIN payment p USING(customer_id)
+       GROUP BY r.customer_id
+       HAVING SUM(p.amount) > 250) AS tab JOIN customer c USING(customer_id)) 
+JOIN rental r1 USING(customer_id)
+LIMIT 10;
+  
+#1.31)
+SELECT DISTINCT(c.customer_id) AS Cliente, c.first_name AS Nombre, c.last_name AS Apellido
+FROM (customer AS c JOIN rental AS r USING(customer_id)) JOIN staff AS s USING(staff_id)
+WHERE s.first_name LIKE 'Jon' and s.last_name LIKE 'Stephens'
+ORDER BY c.customer_id
+LIMIT 10;
+
+#1.32)
+SELECT c.customer_id, c.first_name, c.last_name, DATE(r.rental_date), count(*) 'Cantidad de rentas'
+FROM customer c JOIN rental r USING(customer_id) 
+GROUP BY c.customer_id, DATE(r.rental_date)
+HAVING COUNT(*)>1
+LIMIT 10;
+
+#1.33)
+SELECT s.staff_id, s.first_name, s.last_name, COUNT(*) 'N pagos atendidos'
+FROM staff s JOIN payment p USING(staff_id)
+WHERE p.amount>5
+GROUP BY s.staff_id
+LIMIT 10;
+
+#1.34)
+CREATE OR REPLACE VIEW view_tot_alqXcat_pel AS
+(SELECT c.name AS Categoria, t1.cant as Cantidad
+ FROM (SELECT fc.category_id, COUNT(r.rental_id) AS cant
+	   FROM ((rental AS r JOIN inventory AS i USING(inventory_id)) JOIN film AS f USING(film_id)) JOIN film_category fc USING(film_id)
+	   GROUP BY fc.category_ID) AS t1 JOIN category c USING(category_id));
+
+#1.35)
+SELECT f.film_id, f.title
+FROM film f
+WHERE f.film_id IN (SELECT fa.film_id
+					FROM film_actor fa JOIN actor a USING(actor_id)
+					WHERE a.first_name LIKE '%JENNIFER%' AND a.last_name LIKE '%DAVIS%');
+
+#1.36)
+SELECT c.customer_id ClienteNro, COUNT(DISTINCT(i.film_id)) 'Peliculas Alquiladas'
+FROM (customer c JOIN rental r USING(customer_id)) JOIN inventory i USING(inventory_id)
+GROUP BY c.customer_id
+HAVING COUNT(i.film_id) > 50;
+
+#1.37)
+
