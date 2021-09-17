bd8d10837574b612b49b08dcd5d0c274124250bb
OjedaThomas <90664037+OjedaThomas@users.noreply.github.com>
2021-09-17 20:42:10 -0300

    Add files via upload

---
+++ Carga_Masiva.sql
@@ -0,0 +1,40 @@
+use TP3ejer4;
+
+LOAD DATA LOCAL infile '/home/administrador/tablas/suppliers.txt' into table suppliers fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/parts.txt' into table parts fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/employees.txt' into table employees fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+LOAD DATA LOCAL infile '/home/administrador/tablas/aircraft.txt' into table aircraft fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+  LOAD DATA LOCAL infile '/home/administrador/tablas/faculty.txt' into table faculty fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+  LOAD DATA LOCAL infile '/home/administrador/tablas/emp.txt' into table emp fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+  LOAD DATA LOCAL infile '/home/administrador/tablas/student.txt' into table student fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+  LOAD DATA LOCAL infile '/home/administrador/tablas/class.txt' into table class fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/dept.txt' into table dept fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/catalog.txt' into table catalog fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/certified.txt' into table certified fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/emp.txt' into table emp fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/employees.txt' into table employees fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/enrolled.txt' into table enrolled fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/flights.txt' into table flights fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/parts.txt' into table parts fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ LOAD DATA LOCAL infile '/home/administrador/tablas/sailors.txt' into table sailors fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+  LOAD DATA LOCAL infile '/home/administrador/tablas/works.txt' into table works fields terminated by ','
+ optionally enclosed by '"' lines terminated by '\r\n';
+ 
+ 
