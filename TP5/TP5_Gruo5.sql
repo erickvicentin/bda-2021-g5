9148d426c66fdbc17f087302c5e40e1cdc3195f2
OjedaThomas <90664037+OjedaThomas@users.noreply.github.com>
2021-10-11 15:55:59 -0300

    Update TP5_Gruo5.sql

--- TP5/TP5_Gruo5.sql
+++ TP5/TP5_Gruo5.sql
@@ -1,1 +1,30 @@
+#1_a
+create user 'jPerez'@'%' idintified by '1608';
+create user 'aFernandez'@'%' idintified by '1608';
 
+#1_b
+use sakila;
+
+grant insert, update on sakila.country to 'jPerez'@'%';
+grant insert, update on sakila.country to 'aFernandez'@'%';
+
+show grants for 'jPerez'@'%';
+show grants for 'aFernandez'@'%';
+
+#1_c
+revoke update on sakila.country from 'jPerez'@'%';
+revoke update on sakila.country from 'aFernandez'@'%';
+
+show grants for 'jPerez'@'%';
+show grants for 'aFernandez'@'%';
+
+#1_d
+
+grant create on sakila.* to 'jPerez'@'%';
+
+#1_e
+
+grant select on sakila.address to 'jPerez'@'%';
+grant select on sakila.address to 'aFernandez'@'%';
+
+select * from address where address_id = 1;
